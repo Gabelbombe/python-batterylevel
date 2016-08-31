66171cb3469ed046b6662aa5d3144fd7417eaff4
ehime <dodomeki@gmail.com>
2016-08-31 09:54:47 -0700

    Added scripts dir, rename

--- scripts/batterylevel.py
+++ scripts/batterylevel.py
@@ -16,7 +16,6 @@ charge = b_cur / b_max
 charge_threshold = int(math.ceil(10 * charge))
 
 # Output
-
 total_slots, slots = 10, []
 filled = int(math.ceil(charge_threshold * (total_slots / 10.0))) * u'▸'
 empty = (total_slots - len(filled)) * u'▹'
